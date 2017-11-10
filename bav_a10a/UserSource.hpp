c949327f76a0b300452285145d5a653c5ec438f8
BAVerrecus <33282281+BAVerrecus@users.noreply.github.com>
2017-11-10 21:04:30 +0100

    A-10A Animation Sources

---
+++ bav_a10a/UserSource.hpp
@@ -0,0 +1,59 @@
+﻿class AnimationSources
+{
+		class refuel_door
+		{
+			source = "user";
+			animPeriod = 2;
+			initPhase = 0;
+		};
+		class fuel_probe
+		{
+			source = "user";
+			animPeriod = 0.1;
+			initPhase = 0;
+		};
+		class tvm_locked_hide
+		{
+			source = "user";
+			animPeriod = 0.1;
+			initPhase = 1;
+		};
+		class cam_tracking_hide
+		{
+			source = "user";
+			animPeriod = 0.1;
+			initPhase = 0;
+		};
+		class ejection_seat_hide
+		{
+			source = "user";
+			animPeriod = 0.1;
+			initPhase = 0;
+		};
+		class r_canopy_hide
+		{
+			source = "user";
+			animPeriod = 0.1;
+			initPhase = 0;
+		};
+		class Gatling
+		{
+			source="revolving";
+			weapon="FIR_GAU8";
+		};
+		class Gatling_2
+		{
+			source="revolving";
+			weapon="Gatling_30mm_Plane_CAS_01_F";
+		};
+		class Muzzle_flash
+		{
+			source = "ammorandom";
+			weapon = "FIR_GAU8";
+		};
+		class Muzzle_flash_2
+		{
+			source = "ammorandom";
+			weapon = "Gatling_30mm_Plane_CAS_01_F";
+		};
+};
