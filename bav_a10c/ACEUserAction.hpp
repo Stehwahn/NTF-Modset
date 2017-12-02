c2ff981f71bb282da76f00b67c4d3ddd2f300f6a
BAVerrecus <33282281+BAVerrecus@users.noreply.github.com>
2017-12-02 11:39:20 +0100

    A10C ACE Actions

---
+++ bav_a10c/ACEUserAction.hpp
@@ -0,0 +1,39 @@
+﻿	class ACE_SelfActions 
+		{
+			class extendrefueling 
+			{
+				displayName = "Turn on refuelling";
+				condition = "_player == driver vehicle _player and _target animationPhase ""fuel_probe"" < 0.5 and speed _target > 100";
+				statement = "_target animate [""fuel_probe"",1];_target animate [""refuel_door"",1];";
+				showDisabled = 0;
+				priority = 1;
+				icon = "";
+			};
+			class retractrefueling 
+			{
+				displayName = "Turn off refueling";
+				condition = "_player == driver vehicle _player and _target animationPhase ""fuel_probe"" > 0.5";
+				statement = "_target animate [""fuel_probe"",0];_target animate [""refuel_door"",0];";
+				showDisabled = 0;
+				priority = 1;
+				icon = "";
+			};
+			class rwr_15 
+			{
+				displayName = "$STR_RWR";
+				condition = "(_player == driver vehicle _player || _player == gunner vehicle _player)";
+				statement = "call rwrmod_fnc_interaction";
+				showDisabled = 0;
+				priority = 1;
+				icon = "";
+			};
+			class Aircraft_MFD_Open_N
+			{
+				displayName = "Open I-TGT System";
+				condition = "_target getvariable ""TGT_POD"" == ""yes"" and _player in _target and isengineon _target";
+				statement = "_target execVM ""\FIR_AirWeaponSystem_US\Script\TGTSystem\FIR_AWS_MFD_N_Open.sqf""";
+				showDisabled = 0;
+				priority = 1;
+				icon = "";
+			};
+		};
